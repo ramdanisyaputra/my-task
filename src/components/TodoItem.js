7c4af1cf75595de4d211772213482262e6d094c9
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2023-01-31 06:19:27 +0700

    styling todoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Button from "./Button";
 
 const TodoItem = () => {
     return (
-        <div>
-            todo item
+        <div style={todoItem}>
+            <p>title item</p>
+            <div>
+                <Button/>
+                <Button/>
+            </div>
         </div>
     )
 }
 
 export default TodoItem;
+
+const todoItem = {
+    background: "#2da4f8",
+    color: "#fff",
+    display: "flex",
+    alignItems: "center",
+    height: "3rem",
+    padding: "0 1rem",
+    justifyContent: "space-between",
+    margin: "0.5 rem 0"
+}
